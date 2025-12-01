f3fa242e1162af857083c92654ca9ebdf2d27764
“VictorChidex1” <Donchid.online@gmail.com>
2025-12-01 16:51:17 +0100

    Vercel 2 production

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  // Use the repo name for GitHub Pages, otherwise use root (for Vercel/Local)
-  const isGitHubPages = mode === "production" && !env.VERCEL;
-
-  return {
-    plugins: [react(), tailwindcss()],
-    base: isGitHubPages ? "/lumina-saas/" : "/",
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/",
 });
